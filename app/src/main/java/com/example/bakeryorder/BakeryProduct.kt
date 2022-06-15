640d134a07a83a1632583dcbcee21136b85e7bc7
niranjannlc <niranjannlc10@gmail.com>
2022-06-15 11:03:52 +0545

    initial

---
+++ app/src/main/java/com/example/bakeryorder/BakeryProduct.kt
@@ -0,0 +1,3 @@
+package com.example.bakeryorder
+
+data class BakeryProduct (val name : String, val icing: String, val size: String)
---
+++ app/src/main/java/com/example/bakeryorder/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bakeryorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
